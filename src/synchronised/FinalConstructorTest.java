7ac22932e1188311b4c37ee88b4e7dc24877d44e
zttselena <zttselena@163.com>
2017-12-04 10:56:15 +0800

    final 关键字

---
+++ src/synchronised/FinalConstructorTest.java
@@ -0,0 +1,48 @@
+package synchronised;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FinalConstructorTest {
+	
+	static abstract class A{
+		public A () {
+			display();
+		}
+		
+		public abstract void display();
+	}
+	
+	static class B extends A{
+		private int INT = 100;
+		private final int FINAL_INT = 100;
+		private final Integer FINAL_INTEGER = 100;
+		private String STR1 = "abc";
+		private final String FINAL_STR1 = "abc";
+		private final String FINAL_STR2 = new String("abc");
+		private final List<String> FINAL_LIST = new ArrayList<String>();
+		
+		public B(){
+			super();
+			System.out.println("abc");
+		}
+
+		@Override
+		public void display() {
+			// TODO Auto-generated method stub
+			System.out.println(INT);
+			System.out.println(FINAL_INT);
+			System.out.println(FINAL_INTEGER);
+			System.out.println(STR1);
+			System.out.println(FINAL_STR1);
+			System.out.println(FINAL_STR2);
+			System.out.println(FINAL_LIST);
+		}				
+	}
+	
+	public static void main(String[] args) {
+		new B();
+	}
+
+}
